be752ae72c29d5d5a84dbf4851f8e0705e513c4d
Caio Cruz <covu@gft.com>
2022-10-28 18:49:57 -0300

    Configure ProductService and HttpClient in Startup

--- FrontEnd/Mango.Web/Startup.cs
+++ FrontEnd/Mango.Web/Startup.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Mango.Web.Services;
+using Mango.Web.Services.IServices;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +20,10 @@ namespace Mango.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            SD.ProductAPIBase = Configuration["ServiceUrls:ProductAPI"];
+            services.AddHttpClient<IProductService, ProductService>();
+
+            services.AddScoped<IProductService, ProductService>();
             services.AddControllersWithViews();
         }
 

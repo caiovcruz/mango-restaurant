f019380332818309b9ea2dd61b39b8774051cc84
Caio Cruz <covu@gft.com>
2022-10-28 17:41:15 -0300

    Base Service Interface and API Request Model

---
+++ FrontEnd/Mango.Web/Services/BaseService.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+
+namespace Mango.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto responseModel { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
